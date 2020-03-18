cb821746f4ef26cc3baf5c7bf35960e3dcf5b7a9
Dieter Peirs <github@dieterpeirs.com>
2020-03-18 12:52:55 +0100

    add new data

---
+++ app/components/au-lightbox.js
@@ -0,0 +1,43 @@
+import Component from "@glimmer/component";
+import { action } from "@ember/object";
+import { tracked } from "@glimmer/tracking";
+
+export default class lightbox extends Component {
+  // Variables
+  destinationElementId =  null
+
+  constructor() {
+    super(...arguments);
+
+    // Set wormhole destination
+    if (!this.destinationElementId) {
+      this.destinationElementId = 'ember-vo-appuniversum-wormhole';
+    }
+  }
+
+  // Track modal state
+  @tracked modalOpen = false;
+
+  // Open modal
+  @action
+  openModal() {
+    // Toggle modal view state
+    this.modalOpen = !this.modalOpen;
+
+    // Add body class
+    document.getElementsByTagName('body')[0].classList.add("au-w-modal-open");
+  }
+
+  // Close modal
+  @action
+  closeModal() {
+    // Only run this action when a modal is open
+    if (this.modalOpen) {
+      // Close the modal
+      this.modalOpen = false;
+
+      // Remove body class when modal is disabled
+      document.getElementsByTagName('body')[0].classList.remove("au-w-modal-open");
+    }
+  }
+}
