72526cae40098f3a157e1f0239ab425fcf78e532
Niels Vandekeybus <progster@gmail.com>
2020-04-17 09:30:35 +0200

    ran codemods

--- tests/integration/components/au-criteria-subitem-test.js
+++ tests/integration/components/au-criteria-subitem-test.js
@@ -12,7 +12,7 @@ module('Integration | Component | au-criteria-subitem', function(hooks) {
 
     await render(hbs`<AuCriteriaSubitem />`);
 
-    assert.equal(this.element.textContent.trim(), '');
+    assert.dom(this.element).hasText('');
 
     // Template block usage:
     await render(hbs`
@@ -21,6 +21,6 @@ module('Integration | Component | au-criteria-subitem', function(hooks) {
       </AuCriteriaSubitem>
     `);
 
-    assert.equal(this.element.textContent.trim(), 'template block text');
+    assert.dom(this.element).hasText('template block text');
   });
 });
