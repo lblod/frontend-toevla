c6664154bac8212e8c576cce228a4bdfe4d7ad2c
Sergey Andreev <sergey.andreev@redpencil.io>
2020-06-08 14:42:53 +0200

    changed path to realtive in deploy script

--- deploy-widget.sh
+++ deploy-widget.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 ember build;
 ember build --environment production;
-scp -r /home/sergey/Desktop/toevla-widget/frontend-toevla-master/dist/* root@toevla-dev.s.redpencil.io:/data/app-toevla-embed-experiment/toevla-app/;
+scp -r ./dist/* root@toevla-dev.s.redpencil.io:/data/app-toevla-embed-experiment/toevla-app/;
