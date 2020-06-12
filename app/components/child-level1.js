26219cc50b84bb36ae5d12032f6e0ed6ea9e43ea
Sergey Andreev <sergey.andreev@redpencil.io>
2020-06-12 05:29:26 +0200

    rendered new model elements

--- app/components/child-level1.js
+++ app/components/child-level1.js
@@ -1,7 +1,26 @@
 import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
 
 export default class ChildLevel1Component extends Component {
   get sortedChildren(){
     return this.args.node.children.sortBy("order");
+  }
+
+  temp1=null;
+  temp2=null;
+
+  get hack(){
+    if(this.temp1){
+      return this.temp1
+    }
+    else{
+      if(!this.temp2){
+        this.temp2=this.args.experience.getScoreForTreeNode(this.args.node)
+          .then(function(res){
+            this.temp1=res;
+          }.bind(this));
+      }
+    }
+    return null;
   }
 }
