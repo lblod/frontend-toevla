6715ded0adc86dd0d7f4ff11a3fc73942ddadd3f
Sergey Andreev <sergey.andreev@redpencil.io>
2020-07-10 12:50:58 +0200

    fixed rendering for different depths

--- app/models/entrance.js
+++ app/models/entrance.js
@@ -4,13 +4,13 @@ import { belongsTo, hasMany } from 'ember-data/relationships';
 
 export default class EntranceModel extends Model {
 
-  @attr('boolean') hasMannedDeskl;
-  @attr('boolean') hasTeleloopAtCounterl;
+  @attr('boolean') hasMannedDesk;
+  @attr('boolean') hasTeleloopAtCounter;
   @attr('number') highestThreshold;
   @attr('number') amountOfStairs;
   @attr('number') amountOfSlopes;
-  @attr('boolean') hasEntranceGuttersl;
-  @attr('boolean') hasRevolvingDoorl;
+  @attr('boolean') hasEntranceGutters;
+  @attr('boolean') hasRevolvingDoor;
   @attr('number') doorWidth;
   @attr('number') hasEntranceCheck;
   @attr('number') turningRadiusAtDoor;
