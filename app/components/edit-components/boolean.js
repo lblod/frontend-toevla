9197ba2d8a5e1768737851f76ec13ee0d752a43d
Aad Versteden <madnificent@gmail.com>
2020-07-28 14:41:17 +0200

    Restructure boolean component
    The boolean component has been restructured.  The implementation is
    still the same, yet some logical elements have been moved around.
    This should help first readers.

--- app/components/edit-components/boolean.js
+++ app/components/edit-components/boolean.js
@@ -1,3 +1,4 @@
+import { get } from '@ember/object';
 import Component from '@glimmer/component';
 import { getInstance, property } from '../../utils/path-instances';
 import { tracked } from '@glimmer/tracking';
@@ -5,15 +6,16 @@ import { action } from '@ember/object';
 
 
 export default class EditComponentsBooleanComponent extends Component {
-  constructor(...args){
-    super(...args);
-    this.updateValue();
-  }
-  @action
-  updateValue(){
-    getInstance(this.args.experience, this.args.key).then(function(res){
-      this.value=res[property(this.args.key)];
-    }.bind(this));
-  }
   @tracked value;
+
+  constructor(){
+    super(...arguments);
+    this.initValue();
+  }
+
+  @action
+  async initValue(){
+    const instance = await getInstance(this.args.experience, this.args.key);
+    this.value = get(instance, property(this.args.key));
+  }
 }
