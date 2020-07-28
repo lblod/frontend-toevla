815fb663c3d80650cd49957695fcab21c67876df
Aad Versteden <madnificent@gmail.com>
2020-07-28 14:42:13 +0200

    Centimeters component can be based off Boolean component
    Fetching data for centimeters is the exact same as for Boolean.  We
    can base these on top of each other.  Might be better to create a
    component which sits on top of these various components, but this is
    good enough as a start.

--- app/components/edit-components/centimeters.js
+++ app/components/edit-components/centimeters.js
@@ -1,18 +1,3 @@
-import Component from '@glimmer/component';
-import { getInstance, property } from '../../utils/path-instances';
-import { tracked } from '@glimmer/tracking';
-import { action } from '@ember/object';
+import BooleanComponent from './boolean';
 
-export default class EditComponentsCentimetersComponent extends Component {
-  constructor(...args){
-    super(...args);
-    this.updateValue();
-  }
-  @action
-  updateValue(){
-    getInstance(this.args.experience, this.args.key).then(function(res){
-      this.value=res[property(this.args.key)];
-    }.bind(this));
-  }
-  @tracked value;
-}
+export default class EditComponentsCentimetersComponent extends BooleanComponent { }
