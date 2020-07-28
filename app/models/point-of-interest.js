58905976a499a19c14659782ca299b60eea1c2c2
Aad Versteden <madnificent@gmail.com>
2020-07-28 14:43:06 +0200

    Add mapping and model for Practical Info
    There is now a mapping and a model for the Practical info.  This
    should roughly render correctly in the current state.  Obvious missing
    element is the optional description/comment at the end of this section.

--- app/models/point-of-interest.js
+++ app/models/point-of-interest.js
@@ -4,7 +4,6 @@ import attr from 'ember-data/attr';
 
 export default class PointOfInterestModel extends Model {
   @attr('string') label;
-
   @attr('boolean') hasMovableElectronicPaymentSystem;
   @attr('boolean') wifiAlwaysAvailable;
   @attr('boolean') assistanceForGuideDogs;
@@ -21,7 +20,11 @@ export default class PointOfInterestModel extends Model {
   @attr('boolean') hasMarkingsOrContrastsOnGlassDoors;
   @attr('boolean') extraAttentionGivenToAcoustics;
   @attr('boolean') hasAlternativeEntranceForWheelchair;
-
+  @attr('boolean') hasFreeEntranceForGuide;
+  @attr('boolean') acceptsMuseumPass;
+  @attr('boolean') acceptsUitpas;
+  @attr('boolean') acceptsCityPass;
+  @attr('boolean') acceptsEdc;
   @hasMany('experience') experiences;
   @hasMany('entrance') entrances;
   @hasMany('parking') parkings;
