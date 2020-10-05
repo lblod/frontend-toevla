38597f10fc1be9326a93022826388ffed9e759c1
Aad Versteden <madnificent@gmail.com>
2020-10-05 15:27:56 +0200

    Getting rid of warning on router use
    The router use created a warning.  Getting rid of that.

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,6 +1,10 @@
+import { inject as service } from '@ember/service';
 import Controller from '@ember/controller';
-import { equal } from '@ember/object/computed';
 
-export default Controller.extend({
-  isIndex: equal('currentRouteName', 'index')
-});
+export default class ApplicationController extends Controller {
+  @service router;
+
+  get isIndex() {
+    return this.router.currentRouteName == "index";
+  }
+}
