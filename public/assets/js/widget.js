240930e6f7ae6f759b6946885b13e5d369c6892f
Aad Versteden <madnificent@gmail.com>
2020-10-07 02:39:36 +0200

    Meta tag is not needed anymore, removing

--- public/assets/js/widget.js
+++ public/assets/js/widget.js
@@ -12,7 +12,6 @@
     //add styles and meta tag
 
     var dependencies="\n" +
-        '<meta name="frontend-toevla/config/environment" content="%7B%22modulePrefix%22%3A%22frontend-toevla%22%2C%22environment%22%3A%22production%22%2C%22rootURL%22%3A%22%2F%22%2C%22locationType%22%3A%22auto%22%2C%22EmberENV%22%3A%7B%22FEATURES%22%3A%7B%7D%2C%22EXTEND_PROTOTYPES%22%3A%7B%22Date%22%3Afalse%7D%2C%22_APPLICATION_TEMPLATE_WRAPPER%22%3Afalse%2C%22_DEFAULT_ASYNC_OBSERVERS%22%3Atrue%2C%22_JQUERY_INTEGRATION%22%3Afalse%2C%22_TEMPLATE_ONLY_GLIMMER_COMPONENTS%22%3Atrue%7D%2C%22APP%22%3A%7B%22name%22%3A%22frontend-toevla%22%2C%22version%22%3A%220.0.0%2B56c6dfd2%22%7D%2C%22exportApplicationGlobal%22%3Afalse%7D" />\n' +
         '<link href="https://fonts.googleapis.com/css?family=Lato:400,700,900&display=swap" rel="stylesheet">\n' +
         '<link rel="stylesheet" href="https://use.typekit.net/hdv5dxv.css">\n' +
         '<link integrity="" rel="stylesheet" href="https://qa.toegankelijk.vlaanderen.be/assets/widget/vendor.css">\n' +
