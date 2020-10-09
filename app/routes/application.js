01311ad7af811a5941d158572915ecbf744b2481
Aad Versteden <madnificent@gmail.com>
2020-10-09 12:58:51 +0200

    Correctly render embeddable widget on production builds
    Production builds redirected to the old routed Widget.  This has been
    altered.

--- app/routes/application.js
+++ app/routes/application.js
@@ -2,9 +2,4 @@ import Route from '@ember/routing/route';
 import config from 'frontend-toevla/config/environment';
 
 export default class ApplicationRoute extends Route {
-  beforeModel() {
-    if(config.environment=='production'){
-      this.transitionTo('root');
-    }
-  }
 }
