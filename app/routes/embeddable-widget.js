0236e618910f988db7c7f39ad4c3c0c6265b5e9a
Aad Versteden <madnificent@gmail.com>
2020-10-12 19:00:43 +0200

    Faster loading for embedded widget
    The embedded widget used a slower version of loading.  There's some
    duplication which could be improved by removing routes.

--- app/routes/embeddable-widget.js
+++ app/routes/embeddable-widget.js
@@ -3,26 +3,28 @@ import Route from '@ember/routing/route';
 
 export default class EmbeddableWidgetRoute extends Route {
   @service store;
+  @service nodeScoreStateManager;
 
-  async model( { widget_uri } ) {
-    const uri = decodeURIComponent( widget_uri );
+  async model({widget_id}) {
     const widgets =
-          await this
-          .store
-          .query('widget',
-                 { filter: { ":uri:": uri } },
-                 { include: "point-of-interest.experiences" } );
+          await this.store.query("widget",
+                                 { include: "point-of-interest.experiences" },
+                                 { "filter[:id:]": widget_id } );
     const widget = widgets.firstObject;
-    if( ! widget )
-      return null;
+    const tree = (await this.store.query("tree", { "include": "top-level-nodes" })).firstObject;
 
-    const museum = await widget.pointOfInterest;
-    const experiences = await museum.experiences;
-    if( experiences ) {
-      return {
-        experience: experiences && experiences.firstObject,
-        tree: (await this.store.query('tree', {})).firstObject
-      };
-    }
+    const experience = await widget.get('pointOfInterest.experiences.firstObject');
+
+    window.setTimeout( async () => {
+      await this.nodeScoreStateManager.fetchAll( experience );
+      await this.store.query("tree", { "include": "top-level-nodes.children.children.children.children.children" });
+    }, 1250);
+
+    return {
+      poi: widget.pointOfInterest,
+      widget,
+      experience,
+      tree
+    };
   }
 }
