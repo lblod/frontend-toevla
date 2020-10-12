60dea58f6d6ecf43bdabddb1aae0bccc3b27d56f
Aad Versteden <madnificent@gmail.com>
2020-10-12 12:25:55 +0200

    Use domain environment variable for hosting images
    Hosted images should come from the environment variable, as they do
    now.

--- app/components/list-images.js
+++ app/components/list-images.js
@@ -1,9 +1,11 @@
+import { inject as service } from '@ember/service';
 import Component from '@glimmer/component';
 import { get } from '@ember/object';
-import { computed } from '@ember/object';
 import { sort } from '@ember/object/computed';
 
 export default class ListImagesComponent extends Component {
+  @service env;
+
   //using standard ascending sort
   sortKey = ['order'];
   @sort('images', 'sortKey') sortedImages;
