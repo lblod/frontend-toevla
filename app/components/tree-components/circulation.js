f543f957a4683d3bbccc5a1f61c65fd6a6c6bf2b
Aad Versteden <madnificent@gmail.com>
2020-10-22 17:35:15 +0000

    Released v0.5.4

--- app/components/tree-components/circulation.js
+++
@@ -1,7 +0,0 @@
-import Component from '@glimmer/component';
-
-export default class TreeComponentsCirculationComponent extends Component {
-  get sortedChildren(){
-    return this.args.node.children.sortBy("order");
-  }
-}
