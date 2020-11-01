6e80f1fb2a858b1237eca3e033a55bb337753834
Aad Versteden <madnificent@gmail.com>
2020-11-01 15:58:30 +0100

    v3.16.1...v3.21.2

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,5 +1,5 @@
-import Application from '../app';
-import config from '../config/environment';
+import Application from 'frontend-toevla/app';
+import config from 'frontend-toevla/config/environment';
 import { setApplication } from '@ember/test-helpers';
 import { start } from 'ember-qunit';
 
