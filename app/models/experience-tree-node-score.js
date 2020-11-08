b365d2addb23bbab1031ad3a180aaa0cb551248e
Aad Versteden <madnificent@gmail.com>
2020-11-08 18:31:37 +0100

    Rendering of links when comments are supplied

--- app/models/experience-tree-node-score.js
+++ app/models/experience-tree-node-score.js
@@ -5,6 +5,8 @@ import attr from 'ember-data/attr';
 export default class ExperienceTreeNodeScoreModel extends Model {
   @attr('string') score;
   @attr('string') comment;
+  @attr('string') commentLinkText;
+  @attr('string') commentLinkUrl;
 
   @belongsTo('experience') experience;
   @belongsTo('concept') treeNode;
