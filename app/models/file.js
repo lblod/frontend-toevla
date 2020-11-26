9f8bef25c2c643bb965872c7fbdba54bca231085
Aad Versteden <madnificent@gmail.com>
2020-11-26 14:04:17 +0100

    Support for alt text in images in the caroussel

--- app/models/file.js
+++ app/models/file.js
@@ -3,6 +3,7 @@ import { belongsTo } from 'ember-data/relationships';
 
 export default class FileModel extends Model {
   @attr('string') label;
+  @attr('string') description;
   @belongsTo('point-of-interest') pointOfInterest;
   @attr('number') order;
 }
