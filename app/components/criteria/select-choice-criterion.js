1f4ee2430eea2195c4fd7bb88e211d3f2d108b6b
Aad Versteden <madnificent@gmail.com>
2021-03-26 13:33:30 +0100

    Allow multiple choice to yield no value
    Some options could not render any content.  That's now handled with the
    new input.

--- app/components/criteria/select-choice-criterion.js
+++ app/components/criteria/select-choice-criterion.js
@@ -1,4 +1,10 @@
+import { get } from '@ember/object';
 import Component from '@glimmer/component';
 
 export default class CriteriaSelectChoiceCriterionComponent extends Component {
+  get templateString() {
+    const title = get(this, 'args.value.title') || "geen weergave";
+    const isHidden = ["undefined", "geen weergave"].includes( title.toLowerCase() );
+    return !isHidden && title;
+  }
 }
