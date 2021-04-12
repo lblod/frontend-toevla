d2ce488879a60c6a5c897c88bd376e6a2e787a4b
Dieter Peirs <github@dieterpeirs.com>
2021-04-12 15:54:10 +0200

    add basic dropdown

--- app/components/target-audience.js
+++ app/components/target-audience.js
@@ -1,6 +1,8 @@
 import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 import Component from '@glimmer/component';
+import { guidFor } from '@ember/object/internals';
+import { tracked } from "@glimmer/tracking";
 
 export default class TargetAudienceComponent extends Component {
   @service targetAudience;
@@ -8,5 +10,31 @@ export default class TargetAudienceComponent extends Component {
   @action
   toggleSelected( audience ) {
     this.targetAudience.toggleSelected( audience );
+  }
+
+  // Create a dropdown ID
+  dropdownId = 'dropdown-' + guidFor(this);
+
+  // Track dropdown state
+  @tracked dropdownOpen = false;
+
+  // Open dropdown
+  @action
+  openDropdown() {
+    // Toggle dropdown view state
+    if (!this.dropdownOpen)
+      this.dropdownOpen = true;
+  }
+
+  @action
+  closeDropdown() {
+    // Toggle dropdown view state
+    if (this.dropdownOpen)
+      this.dropdownOpen = false;
+  }
+
+  @action
+  toggleDropdown() {
+    this.dropdownOpen = ! this.dropdownOpen;
   }
 }
