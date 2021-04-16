838111711f4f4793535096526e959707bf87e5a0
Dieter Peirs <github@dieterpeirs.com>
2021-04-16 10:50:01 +0200

    rework filter: simplify interface and duplicate functions

--- app/components/target-audience.js
+++ app/components/target-audience.js
@@ -1,8 +1,6 @@
 import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 import Component from '@glimmer/component';
-import { guidFor } from '@ember/object/internals';
-import { tracked } from "@glimmer/tracking";
 
 export default class TargetAudienceComponent extends Component {
   @service targetAudience;
@@ -10,26 +8,5 @@ export default class TargetAudienceComponent extends Component {
   @action
   toggleSelected( audience ) {
     this.targetAudience.toggleSelected( audience );
-  }
-
-  // Create a dropdown ID
-  dropdownId = 'dropdown-' + guidFor(this);
-
-  // Track dropdown state
-  @tracked dropdownOpen = false;
-
-  // Open dropdown
-  @action
-  openDropdown() {
-    // Toggle dropdown view state
-    if (!this.dropdownOpen)
-      this.dropdownOpen = true;
-  }
-
-  @action
-  closeDropdown() {
-    // Toggle dropdown view state
-    if (this.dropdownOpen)
-      this.dropdownOpen = false;
   }
 }
