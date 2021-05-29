6b50cdbd619dceea229822722a61b40ac7f06ca9
Aad Versteden <madnificent@gmail.com>
2021-05-29 14:41:44 +0200

    Singular build for standalone and embedded version
    We can make do with a singular build rather than two builds.  This has
    the added benefit that we can run a production build everywhere.  That
    makes a lot more sense than splitting/merging the two approaches.

--- app/app.js
+++ app/app.js
@@ -6,9 +6,6 @@ import config from 'frontend-toevla/config/environment';
 export default class App extends Application {
   constructor(...args){
     super(...args);
-    if(config.environment=='production'){
-      this.autoboot=false;
-    }
   }
   modulePrefix = config.modulePrefix;
   podModulePrefix = config.podModulePrefix;
