818db35961fad2f51e0bd3ec575583b6ed6b15d3
Aad Versteden <madnificent@gmail.com>
2021-06-18 15:34:34 +0200

    Correctly decide whether boolean criterion will be shown or not
    We updated the logic for rendering booleans when they have not been
    entered in detail.  The logic has now been copied over.  There is a lot
    of room to remove duplication here, we should do that soon-ish.

--- app/helpers/select-boolean-criterion.js
+++ app/helpers/select-boolean-criterion.js
@@ -4,9 +4,12 @@ import Criterion from '../utils/criterion';
 class Matcher extends Criterion {
 
   get matchedIndex() {
-    return this.value ? 1 : 2;
+    if (this.value === true)
+      return 1;
+    else if (this.value === false)
+      return 2;
+    else return 3;
   }
-
 }
 
 function booleanCriterion(treeNode, value) {
